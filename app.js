56937216e3cd2aaa75fcd7eaa90f505edef38860
attribbuttes <electronic.circus@gmail.com>
2022-02-05 22:55:41 -0300

    problemas

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => {
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Servidor funcionando puerto 3000');
 });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html');
 });
+
